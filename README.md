f23fcdf4864d9120e8f9d546a3edc6d8408e165a
QUERY-GANI <ahmadxgani01@gmail.com>
2021-07-28 08:53:00 +0700

    update script

---
+++ README.md
@@ -0,0 +1,9 @@
+## default dir is set to your home directory
+
+### You must use regex for unique char, see example below:
+
+```bash
+node .
+Enter the directory: /test
+Enter the character you want to remove from name file: \:
+```
---
+++ index.js
@@ -0,0 +1,20 @@
+// My youtube playlist/computer science
+
+const fs = require("fs")
+const path = require("path")
+const readline = require("readline")
+
+const rl = readline.createInterface(process.stdin, process.stdout)
+
+rl.question("Enter the directory: ", (inputDir) => {
+  if (!inputDir) throw new Error("input can't be empty")
+  const fullDir = path.join(process.env.HOME, inputDir)
+  const files = fs.readdirSync(fullDir)
+
+  rl.question("Enter the character you want to remove from name file: ", (rename) => {
+    files.map((file) => {
+      fs.renameSync(path.join(fullDir, file), path.join(fullDir, file.replace(new RegExp(rename), "")))
+    })
+    rl.close()
+  })
+})
