bb0dbab3261f55a6c3528984f8e92a32d600019b
query-gani <ahmadxgani01@gmail.com>
2022-07-20 18:36:35 +0700

    update

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ const path = require("path")
 const readline = require("readline")
 
 const rl = readline.createInterface(process.stdin, process.stdout)
-
 rl.question("Enter the directory: ", (inputDir) => {
   if (!inputDir) throw new Error("input can't be empty")
+  if (process.argv[2] === "prune") {
+  } else {
+  }
   const fullDir = path.join(process.env.HOME, inputDir)
   const files = fs.readdirSync(fullDir)
 
   rl.question("Enter the character you want to remove from name file: ", (rename) => {
-    files.map((file) => {
-      fs.renameSync(path.join(fullDir, file), path.join(fullDir, file.replace(new RegExp(rename, "g"), "")))
+    rl.question("Enter the character you want to change from removed character: ", (result) => {
+      files.map((file) => {
+        fs.renameSync(path.join(fullDir, file), path.join(fullDir, file.replace(new RegExp(rename, "g"), result)))
+      })
+      rl.close()
     })
-    rl.close()
   })
 })
